0bc770aa130d08dcb346030b943fedc68399d31e
Nikalexis Nikos <nikalexis@gmail.com>
2023-02-14 12:07:09 +0200

    Adds partial panel page

--- src/django_adminlte_ui/views/panel.py
+++ src/django_adminlte_ui/views/panel.py
@@ -1,11 +1,26 @@
 from django_adminlte_ui.utils import avatar_url
 from django_htmx_ui.utils import ContextProperty
 from django_htmx_ui.views.generic import PrivateTemplateView
+from django_htmx_ui.views.mixins import PartialMixin
 
 
-class PanelPage(PrivateTemplateView):
-    template_root = 'django_adminlte_ui/panel/panel.html'
+class CommonMixin:
+
+    def response_panel_page(self, url):
+        self.response_location(
+            url,
+            target='#page',
+            swap='innerHTML',
+        )
 
     @ContextProperty
     def avatar_url(self):
         return avatar_url(self.request.user)
+
+
+class PanelPage(CommonMixin, PrivateTemplateView):
+    template_root = 'django_adminlte_ui/panel/panel.html'
+
+
+class PanelPartialPage(CommonMixin, PartialMixin, PrivateTemplateView):
+    pass
