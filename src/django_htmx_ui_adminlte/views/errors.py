27c102085141fea612b05373c11b0ba54557e2c9
Nikalexis Nikos <nikalexis@gmail.com>
2024-02-15 20:58:29 +0200

    Adds 500, 404 http status and ui messages

--- src/django_htmx_ui_adminlte/views/errors.py
+++ src/django_htmx_ui_adminlte/views/errors.py
@@ -4,6 +4,28 @@ from django_htmx_ui_adminlte.views.welcome import WelcomeOrigin
 class ErrorNotFound(WelcomeOrigin):
     template_name = 'django_htmx_ui_adminlte/errors/404.html'
 
+    def response_prepare(self, response):
+        response = super().response_prepare(response)
+        response.status_code = 404
+        return response
+
+    def on_get(self, request, *args, **kwargs):
+        self.message_warning('404 - Oops, object not found!')
+
+    def on_post(self, request, *args, **kwargs):
+        self.message_warning('404 - Oops, object not found!')
+
 
 class ErrorServer(WelcomeOrigin):
     template_name = 'django_htmx_ui_adminlte/errors/500.html'
+
+    def response_prepare(self, response):
+        response = super().response_prepare(response)
+        response.status_code = 500
+        return response
+
+    def on_get(self, request, *args, **kwargs):
+        self.message_error('500 - Oops, server error for that request!')
+
+    def on_post(self, request, *args, **kwargs):
+        self.message_error('500 - Oops, server error for that request!')
